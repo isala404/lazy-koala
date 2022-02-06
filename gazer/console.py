c937b97ccea4a31527232ac0528ae10056c33edb
Isala Piyarisi <code@isala.me>
2022-02-06 23:02:00 +0530

    Setup filters

---
+++ gazer/console.py
@@ -0,0 +1,76 @@
+import curses
+import time
+from gazer import Gazer
+
+
+def draw_menu(stdscr: curses.window):
+    k = 0
+    cursor_x = 0
+    cursor_y = 0
+
+    # Clear and refresh the screen for a blank canvas
+    stdscr.clear()
+    stdscr.refresh()
+    stdscr.nodelay(True)
+    # curses.delay_output(100)
+
+    # Start colors in curses
+    curses.start_color()
+    curses.init_pair(1, curses.COLOR_CYAN, curses.COLOR_BLACK)
+    curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
+    curses.init_pair(3, curses.COLOR_BLACK, curses.COLOR_WHITE)
+
+    # Loop where k is the last character pressed
+    while k != ord('q'):
+        try:
+            # Initialization
+            stdscr.clear()
+            height, width = stdscr.getmaxyx()
+
+            if k == curses.KEY_DOWN:
+                cursor_y = cursor_y + 1
+            elif k == curses.KEY_UP:
+                cursor_y = cursor_y - 1
+            elif k == curses.KEY_RIGHT:
+                cursor_x = cursor_x + 1
+            elif k == curses.KEY_LEFT:
+                cursor_x = cursor_x - 1
+
+            cursor_x = max(0, cursor_x)
+            cursor_x = min(width - 1, cursor_x)
+
+            cursor_y = max(0, cursor_y)
+            cursor_y = min(height - 1, cursor_y)
+
+            statusbarstr = "Press 'q' to exit | STATUS BAR | Pos: {}, {}".format(cursor_x, cursor_y)
+
+            stdscr.addstr(0, 0, "Requests", curses.color_pair(1))
+            stdscr.addstr(2, 0, gazer.request_log_text(), curses.color_pair(1))
+            stdscr.addstr(15, 0, gazer.syn_backlog_text(), curses.color_pair(1))
+
+            # Render status bar
+            stdscr.attron(curses.color_pair(3))
+            stdscr.addstr(height - 1, 0, statusbarstr)
+            stdscr.addstr(height - 1, len(statusbarstr), " " * (width - len(statusbarstr) - 1))
+            stdscr.attroff(curses.color_pair(3))
+
+            stdscr.move(cursor_y, cursor_x)
+
+            # Refresh the screen
+            stdscr.refresh()
+
+            # Wait for next input
+            k = stdscr.getch()
+            time.sleep(1 / 30)
+        except:
+            # Refresh the screen
+            stdscr.refresh()
+
+            # Wait for next input
+            k = stdscr.getch()
+
+
+gazer = Gazer(console=True)
+gazer.poll_data_in_bg()
+
+curses.wrapper(draw_menu)
--- gazer/gazer.py
+++ gazer/gazer.py
@@ -5,14 +5,17 @@ import pandas as pd
 from bcc import BPF
 from socket import inet_ntop, AF_INET
 from struct import pack
+from config import config_watcher
 
 
 class Gazer:
     request_df = pd.DataFrame(columns=["PID", "COMM", "LADDR", "LPORT", "RADDR", "RPORT", "TX_KB", "RX_KB", "MS"])
     syn_df = pd.DataFrame(columns=["backlog", "slot", "saddr", "lport", "value", "outdated"])
     bpf_text = ""
+    console_mode = False
 
-    def __init__(self):
+    def __init__(self, console_mode=False):
+        self.console_mode = console_mode
         with open('bpf.c', 'r') as f:
             bpf_text = f.read()
 
@@ -30,36 +33,54 @@ class Gazer:
 
     def ipv4_request_event(self, cpu, data, size):
         event = self.b["ipv4_events"].event(data)
-        self.request_df = self.request_df.append({
-            "PID": event.pid,
-            "COMM": event.task.decode('utf-8', 'replace'),
-            "LADDR": inet_ntop(AF_INET, pack("I", event.saddr)),
-            "LPORT": event.ports >> 32,
-            "RADDR": inet_ntop(AF_INET, pack("I", event.daddr)),
-            "RPORT": event.ports & 0xffffffff,
-            "TX_KB": event.tx_b,
-            "RX_KB": event.rx_b,
-            "MS": float(event.span_us) / 1000,
-        }, ignore_index=True)
-        self.request_df = self.request_df[-10:]
+        LADDR = inet_ntop(AF_INET, pack("I", event.saddr))
+        RADDR = inet_ntop(AF_INET, pack("I", event.daddr))
+
+        if LADDR in config_watcher.config and RADDR in config_watcher.config:
+            # TODO: Write to prometheus
+            pass
+
+        if self.console_mode:
+            self.request_df = self.request_df.append({
+                "PID": event.pid,
+                "COMM": event.task.decode('utf-8', 'replace'),
+                "LADDR": LADDR,
+                "LPORT": event.ports >> 32,
+                "RADDR": RADDR,
+                "RPORT": event.ports & 0xffffffff,
+                "TX_KB": event.tx_b,
+                "RX_KB": event.rx_b,
+                "MS": float(event.span_us) / 1000,
+            }, ignore_index=True)
+            self.request_df = self.request_df[-10:]
 
     def poll_requests(self):
         while True:
             self.b.perf_buffer_poll()
+
+    def poll_kube_api(self):
+        pass
 
     def poll_syn_backlog(self):
         while True:
             data = self.syn_backlog_buffer.items()
             self.syn_df["outdated"] = True
             for row in data:
-                self.syn_df = self.syn_df.append({
-                    "backlog": row[0].backlog,
-                    "slot": row[0].slot,
-                    "saddr": inet_ntop(AF_INET, pack("I", row[0].saddr)),
-                    "lport": row[0].lport,
-                    "value": row[1].value,
-                    "outdated": False,
-                }, ignore_index=True)
+                saddr = inet_ntop(AF_INET, pack("I", row[0].saddr))
+
+                if saddr in config_watcher.config:
+                    # TODO: Write to prometheus
+                    pass
+
+                if self.console_mode:
+                    self.syn_df = self.syn_df.append({
+                        "backlog": row[0].backlog,
+                        "slot": row[0].slot,
+                        "saddr": inet_ntop(AF_INET, pack("I", row[0].saddr)),
+                        "lport": row[0].lport,
+                        "value": row[1].value,
+                        "outdated": False,
+                    }, ignore_index=True)
             self.syn_backlog_buffer.clear()
             time.sleep(5)
 
@@ -98,3 +119,7 @@ class Gazer:
         poll_requests = threading.Thread(target=self.poll_requests, args=())
         poll_requests.daemon = True
         poll_requests.start()
+
+        if not self.console_mode:
+            poll_syn_backlog.join()
+            poll_requests.join()
