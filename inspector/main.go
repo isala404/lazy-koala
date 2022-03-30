27c42086caec1023e4f55389b005b89505f57af1
Isala Piyarisi <code@isala.me>
2022-03-30 10:27:12 +0530

    feat(inspector): Integrated proxy with kubernetes

--- inspector/main.go
+++ inspector/main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"crypto/tls"
+	"crypto/x509"
 	"embed"
+	"fmt"
 	"io/fs"
+	"io/ioutil"
 	"log"
+	"net"
 	"net/http"
 	"net/http/httputil"
 	"net/url"
@@ -12,7 +17,11 @@ import (
 	"strings"
 
 	"github.com/gorilla/mux"
-	"github.com/rs/cors"
+)
+
+const (
+	tokenFile  = "/var/run/secrets/kubernetes.io/serviceaccount/token"
+	rootCAFile = "/var/run/secrets/kubernetes.io/serviceaccount/ca.crt"
 )
 
 //go:embed ui/*
@@ -44,19 +53,60 @@ func clientHandler() http.Handler {
 }
 
 func main() {
-	kube_endpoint, _ := url.Parse("http://127.0.0.1:8001/")
-	prom_endpoint, _ := url.Parse("http://127.0.0.1:9090/")
+
+	// Setup proxy data
+	token, err := ioutil.ReadFile(tokenFile)
+	if err != nil {
+		log.Println("failed to read the token from kubernetes secrets")
+	}
+	CAData, err := ioutil.ReadFile(rootCAFile)
+	if err != nil {
+		log.Println("failed to read the CA Data from kubernetes secrets")
+	}
+
+	// Get the SystemCertPool, continue with an empty pool on error
+	rootCAs, _ := x509.SystemCertPool()
+	if rootCAs == nil {
+		rootCAs = x509.NewCertPool()
+	}
+
+	// Append our cert to the system pool
+	if ok := rootCAs.AppendCertsFromPEM(CAData); !ok {
+		log.Println("failed to append k8s custom CA, using system certs only")
+	}
+
+	if err != nil {
+		log.Println("it seems like operator is not running in side cluster, kube-api proxy will not operate as intended")
+	}
+
+	proxyTransport := &http.Transport{TLSClientConfig: &tls.Config{
+		RootCAs: rootCAs,
+	}}
+
+	kube_endpoint, _ := url.Parse("https://" + net.JoinHostPort(os.Getenv("KUBERNETES_SERVICE_HOST"), os.Getenv("KUBERNETES_SERVICE_PORT")))
+
+	prom_endpoint, _ := url.Parse(os.Getenv("PROMETHEUS_ENDPOINT"))
 	r := mux.NewRouter()
 
 	r.PathPrefix("/k8s").HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("Proxying %s Request from %s to KubeAPI on %s\n", r.Method, r.RemoteAddr, r.URL.Path)
+
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Private-Network", "true")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+		w.Header().Set("Access-Control-Allow-Methods", "OPTIONS, GET, POST, DELETE")
+		if r.Method == http.MethodOptions {
+			w.WriteHeader(204)
+			return
+		}
 		proxy := httputil.NewSingleHostReverseProxy(kube_endpoint)
+		proxy.Transport = proxyTransport
+		r.Header.Add("Authorization", fmt.Sprintf("Bearer %s", string(token)))
 
 		r.URL.Path = "/" + strings.Join(strings.Split(r.URL.Path, "/")[2:], "/")
 
-		log.Printf("Proxying Request from %s to KubeAPI on %s\n", r.RemoteAddr, r.URL.Path)
-
 		proxy.ServeHTTP(w, r)
-	})
+	}).Methods(http.MethodGet, http.MethodPost, http.MethodDelete, http.MethodOptions)
 
 	r.PathPrefix("/prom").HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		proxy := httputil.NewSingleHostReverseProxy(prom_endpoint)
@@ -66,22 +116,16 @@ func main() {
 		log.Printf("Proxying Request from %s to prometheus on %s\n", r.RemoteAddr, r.URL.Path)
 
 		proxy.ServeHTTP(w, r)
-	})
+		// fmt.Printf("proxy.ModifyResponse: %v\n", proxy.ModifyResponse)
+	}).Methods(http.MethodGet, http.MethodOptions)
 
 	r.PathPrefix("/").Handler(clientHandler())
 
-	corsOpts := cors.New(cors.Options{
-		AllowedOrigins: []string{"*"},
-		AllowedMethods: []string{
-			http.MethodGet,
-			http.MethodDelete,
-			http.MethodPost,
-		},
-	})
+	r.Use(mux.CORSMethodMiddleware(r))
 
 	log.Println("Client app started on port :8090")
 
-	if err := http.ListenAndServe(":8090", corsOpts.Handler(r)); err != http.ErrServerClosed {
+	if err := http.ListenAndServe(":8090", r); err != http.ErrServerClosed {
 		log.Fatal("Receiver webserver crashed: %s", err)
 		os.Exit(1)
 	}
---
+++ scripts/loadTemplete.py
@@ -0,0 +1,49 @@
+import json
+import random
+
+services = list(range(1,10))
+
+random.shuffle(services)
+
+templete = {
+    "designation": None,
+    "probability": 50,
+    "faults": {
+        "before": [{"type": "latency", "args": { "delay": 600 }}],
+        "after": [{"type": "latency", "args": { "delay": 600 }}]
+    },
+    "routes": None
+}
+
+
+def create_route(k, last_designation=None):
+    path = None
+    
+    if k <= 15:
+        k+=1
+        route = templete.copy()
+        route['designation'] = f"service_{services[random.randint(0,len(services)-1)]}"
+        while route['designation'] == last_designation:
+            route['designation'] = f"service_{services[random.randint(0,len(services)-1)]}"
+        route['probability'] = random.randint(50, 100)
+        route['faults']['before'][0]['args']['delay'] = random.randint(100, 400)
+        route['faults']['after'][0]['args']['delay'] = random.randint(100, 400)
+        route['routes'] = [create_route(k,  route['designation'])]
+        if route['routes'][0] == None:
+            route['routes'] = None
+        # for i in range(random.randint(0,2)):
+        #     k+=1
+        #     sub_route = create_route(k)
+        #     if sub_route:
+        #         route['routes'].append(sub_route)
+        #     else:
+        #         route['routes'] = None
+        #         break
+        
+        path = route
+   
+    return path
+
+out_file = open("routes.json", "w")
+  
+json.dump(create_route(0), out_file, separators=(',', ':'))
